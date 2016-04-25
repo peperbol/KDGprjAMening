51d44560e09cd4c1a2dc94745453f51783e03ccf
Pepijn Willekens <pepijnwillekens@gmail.com>
2016-04-25 13:34:35 +0200

    made slider work

---
+++ app/aMening/Assets/scripts/SwipeInput.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeInput : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    public float tweenMultipier;
+    float split = 0.5f;
+    public Material splitMaterial;
+    public float snapSplit = 0.15f;
+    public float confirmSplit = 0.005f;
+    public float Split
+    {
+        get { return split; }
+        set
+        {
+            split = Mathf.Clamp(value,0,1);
+            if (splitMaterial)
+            {
+                splitMaterial.SetFloat("_Split", split);
+            }
+        }
+    }
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Split += Input.touches[0].deltaPosition.x / Screen.width;
+            Debug.Log(Split);
+        }
+        else
+        { 
+            float delta = Mathf.Abs(Split - 0.5f) ;
+            if (delta < 0.5f - snapSplit)
+            {
+                delta *= tweenMultipier;
+                if (Split > 0.5f)
+                {
+                    Split -= delta;
+                }
+                else
+                {
+                    Split += delta;
+                }
+            }
+            else if (delta < 0.5f - confirmSplit)
+            {
+
+                delta *= 0.5f - tweenMultipier;
+                if (Split > 0.5f)
+                {
+                    Split += delta;
+                }
+                else
+                {
+                    Split -= delta;
+                }
+            }
+            else
+            {
+                Split = Mathf.Round(Split);
+                // confirm
+            }
+        }
+    }
+}
---
+++ app/aMening/Assets/shaders/SplitScreen.shader
@@ -0,0 +1,66 @@
+﻿Shader "AMening/SplitScreen"
+{
+	Properties
+	{
+		_MainTex1 ("Texture 1", 2D) = "white" {}
+		_MainTex2 ("Texture 2", 2D) = "white" {}
+		_Split("Split", Range(0,1)) = 0.5
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex1;
+			sampler2D _MainTex2;
+			float _Split;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col;
+				if (i.uv.x < _Split)
+				{
+					i.uv.x = 0.5f - _Split / 2 + i.uv.x;
+					col = tex2D(_MainTex1, i.uv);
+				}
+				else {
+
+					i.uv.x = 0.5f - (1 - _Split) / 2 + i.uv.x - _Split;
+					col = tex2D(_MainTex2, i.uv);
+				}
+				// sample the texture
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
