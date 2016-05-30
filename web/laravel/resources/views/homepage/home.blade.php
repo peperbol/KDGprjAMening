6725eefb1cd773d9a81e96f980488a73d1841e59
Pepijn Willekens <pepijnwillekens@gmail.com>
2016-05-30 16:56:52 +0200

    fixed map

--- web/laravel/resources/views/homepage/home.blade.php
+++ web/laravel/resources/views/homepage/home.blade.php
@@ -4,12 +4,12 @@
         <title>A-Mening projecten</title>
 
         <!-- CSS And JavaScript -->
-        
+
         <!--<link href="css/style.css" rel="stylesheet">-->
         <!-- moet op onderstaande manier zodat de css accessible is in alle views-->
         <!--<link href="{{ asset('css/homepage_style.css') }}" rel="stylesheet" type="text/css" >-->
-        
-        
+
+
         <meta charset="UTF-8">
         <title>A Mening</title>
         <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css ">
@@ -23,19 +23,19 @@
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/icon_images/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('images/icon_images/favicon-96x96.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/icon_images/favicon-16x16.png') }}">
-        
+
     </head>
 
     <body ng-controller="ProjectController" ng-init="Show_project_info({{$projects[0]->id_project}})">
-       
+
        <!--
        <h1>A-Mening - projecten in Antwerpen</h1>
-       
+
        <div>
            <img src="images/icon_images/Google_Play_logo.png" alt="test">
        </div>
         <div class="left_box">
-            
+
             <div class="map">
 
             </div>
@@ -47,74 +47,74 @@
                         <li ng-click="Show_project_info({{$project->id_project}})">{{ $project->name }}</li>
                     @endforeach
                 </ul>
-                
+
             </div>
-            
+
         </div>
-        
+
         <div class="right_box">
             <h3>@{{project_Name}}</h3>
             <p>@{{project_Description}}</p>
             <p>@{{project_Startdate}}</p>
             <!--<img src="images/project_images/@{{project_Imagepath}}" alt="xx">--><!--
-            
+
             <div ng-repeat="fase in timelineArray">
-                  
+
                     <a ng-click="Show_fase_info(fase.id_project_phase)">@{{fase.faseWithNr}}</a>
-                    
+
             </div>
-            
+
             <h3>@{{current_Fase_Name}}</h3>
             <p>@{{current_Fase_description}}</p>
             <p>@{{current_Fase_enddate}}</p>
-            
-            
-            
-            
+
+
+
+
             <form name="questionForm" ng-submit="SendAnswer()" ng-show="form_questions_shown" novalidate>
                 <div ng-repeat="question in questionsPhase">
-                  
+
                     <p>@{{question.questiontext}}</p>
-                    
+
                     <input type="radio" name="@{{question.id_question}}" ng-model="question.answer" value="0" id="answL@{{question.id_question}}">
                     <label for="answL@{{question.id_question}}">@{{question.leftlabel}}</label> <br>
                     <input type="radio" name="@{{question.id_question}}" ng-model="question.answer" value="1" id="answR@{{question.id_question}}">
                     <label for="answR@{{question.id_question}}">@{{question.rightlabel}} </label> <br>
-                    
+
                 </div>
-                    
+
                     <textarea id="comment_sectie" name="commentFase" placeholder="Commentaar" ng-model="comment"></textarea>
-                
+
                 <button type="submit">STUUR IN</button>
-                
+
             </form>
-            
-            
-            
+
+
+
             <div ng-show="first_comment_shown">
                 <p>@{{first_comment.comment}}</p>
                 <p>Bericht geplaatst op @{{first_comment.date}} om @{{first_comment.time}}</p>
             </div>
-            
+
             <div ng-repeat="comment in commentsArray">
-                 
+
                 <div ng-show="extra_comments_shown">
                     <p>@{{comment.comment}}</p>
                     <p>Bericht geplaatst op @{{comment.date}} om @{{comment.time}}</p>
                 </div>
-                    
+
             </div>
-            
+
             <button ng-show="button_show_more_shown" ng-click="Show_extra_comments()">@{{button_show_more_text}}</button>
-            
+
         </div>
-        
+
         <!--
         <div ng-controller="projController as proj">
             <form ng-submit="proj.sendAnswer()">
-                
+
                 <input type="text" name="test" id="test" required ng-model="proj.inputje">
-           
+
                    <button type="submit">toevoegen</button>
             </form>
         </div>
@@ -127,9 +127,9 @@
             @endif--}}
         </div>
         -->
-        
+
         <!-----------    Pepijn's deel        --------->
-        
+
         <div class="leftSide">
             <a class="logo" href="https://www.antwerpen.be/nl/home">
               <img width="100%" heigth="100%" src="images/icon_images/logo.jpg">
@@ -154,10 +154,10 @@
                 </ul>
             </label>
           </div>
-        
-        
+
+
         <!--    right side    -->
-        
+
         <div class="rightSide">
             <div class="header" style="background-image: url('images/project_images/@{{current_Fase_imagepath}}');"></div>
             <div class="contentWrapper">
@@ -184,25 +184,25 @@
                         <label for="answR@{{question.id_question}}">@{{question.rightlabel}}</label>
                       </li>
                     </ul>
-                   
+
                   </div>
 
                   <textarea class="feedbackInput" name="commentFase" placeholder="Wij horen graag jouw mening!" ng-model="comment"></textarea>
-                  
+
                   <div class="submit_confirmation">
                       <p></p>
                   </div>
-                  
+
                   <button class="stuurIn" type="submit">STUUR IN</button>
-                  
-                  
+
+
               </form>
 
-             
-             
+
+
               <h3>Reacties</h3>
               <div class="comments">
-               
+
                 <div ng-show="first_comment_shown">
                   <p class="bodytext">
                     @{{first_comment.comment}}
@@ -214,9 +214,9 @@
                     <span class="highlight">@{{first_comment.time}}</span>
                   </p>
                 </div>
-                
+
                 <div ng-repeat="comment in commentsArray">
-                 
+
                     <div ng-show="extra_comments_shown">
                         <p class="bodytext">
                             @{{comment.comment}}
@@ -228,33 +228,33 @@
                             <span class="highlight">@{{comment.time}}</span>
                         </p>
                     </div>
-                    
+
                 </div>
-            
-                
+
+
               </div>
               <div class="center">
                 <button class="morecomments" ng-show="button_show_more_shown" ng-click="Show_extra_comments()">@{{button_show_more_text}}</button>
               </div>
-              
+
               <p ng-hide="first_comment_shown">Voorlopig zijn er nog geen reacties geplaatst.</p>
-              
+
             </div>
 
           </div>
-        
-        
-        
+
+
+
         <div class="timelineContainer">
             <ul class="timeline">
-             
-             
+
+
               <li ng-repeat="fase in timelineArray" ng-click="Show_fase_info(fase.id_project_phase, fase.faseWithNr)" ng-class="{ 'currentPhase': fase.faseWithNr!=timelineArray.length-1 , 'fase': fase.faseWithNr==timelineArray.length-1, 'active': Phase_selected(fase.faseWithNr)  }">
                 <h2 class="faseHead">FASE</h2>
                 <h2 class="faseHead">@{{fase.faseWithNr}}</h2>
                 <p ng-show="fase.faseWithNr!=timelineArray.length-1">@{{fase.enddate}}</p>
               </li>
-              
+
             </ul>
 
             <div class="fase playstore">
@@ -263,24 +263,20 @@
               </a>
             </div>
         </div>
-        
-            
-            
-        
-    
-    
-    <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+
+
+
+
+
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDtRQT9vIjQs-drmitlleXzqi5xgtYH3L8" async defer></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="{{ asset('js/mainpage.js') }}" type="text/javascript"></script>
-    
-    
 
-   
-   
+
+
+
+
     </body>
 </html>
-
-
-
-
